03005601b2f15767827e234c3f0ebf0d388bcd18
ljc2177 <108085595+ljc2177@users.noreply.github.com>
2023-04-24 19:23:37 -0400

    Revert to server build

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/the-little-apple/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/the-little-apple/" : "/",
   devServer: {
     allowedHosts: 'all',
-    overlay: {
-      warnings: false,
-      errors: true
-    },
+    // overlay: {
+    //   warnings: false,
+    //   errors: true
+    // },
     client: {
       webSocketURL: 'wss://localhost:8080/'
     },
